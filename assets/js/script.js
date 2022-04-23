4dfffd4ca5674d771a1cf87a9e6bef0323abe473
ignacioleonm <ignacio.leonm@outlook.com>
2022-04-23 17:08:37 -0400

    aplicando tooltip

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// tooltips js para realizar la animación
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
