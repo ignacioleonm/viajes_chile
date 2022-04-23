0bfa6d5abde386b3e0366c7b89e3a4591cafa1d9
ignacioleonm <ignacio.leonm@outlook.com>
2022-04-23 17:25:29 -0400

    aplicación de jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+  
+    $("#fa-logo1").click(function () {
+      $("#p-lorem1").toggle(100);
+    });
+  
+    $("#fa-logo2").click(function () {
+      $("#p-lorem2").toggle(100);
+    });
+  
+    $("#fa-logo3").click(function () {
+      $("#p-lorem3").toggle(100);
+    });
+    
+  
+    });
+
+
+
+
 // tooltips js para realizar la animación
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
